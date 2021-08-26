2a39a711d49fca91ac62f8ef666c23efa1476735
Georgie Dignan <georgiedignan@gmail.com>
2021-08-26 19:37:59 +1000

    Bootstrap
    Added bootstrap links into base.html

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -1,15 +1,28 @@
 from django import forms
-from django.forms import ModelForm
 from .models import NewsStory
+from django.forms import ModelForm, TextInput, Textarea
+
 
 #allow Django to infer from all the form fields and validation from a model
-class StoryForm(ModelForm):
+class StoryForm(forms.ModelForm):
     class Meta:
         #list of fields to be included in the field
         model = NewsStory
         fields = ['title','pub_date','content','image_url']
         widgets = {
             'pub_date': forms.DateInput(format=('%m/%d/%Y'),attrs={'class':'form-control', 'placeholder':'Selecta date','type':'date'}),
+
+            'title': forms.TextInput(attrs={
+                'class': "form-control",
+                }),
+
+            'image_url': forms.TextInput(attrs={
+                'class': "form-control",
+                }),
+
+            'content': forms.Textarea(attrs={
+                'id': "WriteStory",
+                }),
         }
       
 
